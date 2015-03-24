0e718e570625afc2b906d922322f76775abb781d
Vagn Scott <vagnscott@gmail.com>
2015-03-24 11:38:05 -0400

    finished hello exercise.

---
+++ D02_hello/hello.cc
@@ -0,0 +1,46 @@
+#include <Wt/WApplication>
+#include <Wt/WBreak>
+#include <Wt/WContainerWidget>
+#include <Wt/WLineEdit>
+#include <Wt/WPushButton>
+#include <Wt/WText>
+
+class HelloApplication : public Wt::WApplication
+{
+public:
+    HelloApplication(const Wt::WEnvironment& env);
+
+private:
+    Wt::WLineEdit *nameEdit_;
+    Wt::WText *greeting_;
+
+    void greet();
+};
+
+HelloApplication::HelloApplication(const Wt::WEnvironment& env)
+    : Wt::WApplication(env)
+{
+    setTitle("Hello world");
+
+    root()->addWidget(new Wt::WText("Your name, please ? "));
+    nameEdit_ = new Wt::WLineEdit(root());
+    Wt::WPushButton *button = new Wt::WPushButton("Greet me.", root());
+    root()->addWidget(new Wt::WBreak());
+    greeting_ = new Wt::WText(root());
+    button->clicked().connect(this, &HelloApplication::greet);
+}
+
+void HelloApplication::greet()
+{
+    greeting_->setText("Hello there, " + nameEdit_->text());
+}
+
+Wt::WApplication *createApplication(const Wt::WEnvironment& env)
+{
+    return new HelloApplication(env);
+}
+
+int main(int argc, char **argv)
+{
+    return Wt::WRun(argc, argv, &createApplication);
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # 45_wt
 experiment with wt c++ web framework
+
+README.md	this file
+01_prep		install script
+
+D02_hello	hello.cc example w/ Makefile
+
+D03_hangman	next assignment
